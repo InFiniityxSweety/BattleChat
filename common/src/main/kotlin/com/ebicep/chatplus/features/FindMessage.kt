264490e6dffcb3d2ad12dc982e4d6df083d206b2
ebicep <ebicep314@gmail.com>
2025-02-28 00:13:23 -0500

    feat: started find message regex + highlighting matched text

--- common/src/main/kotlin/com/ebicep/chatplus/features/FindMessage.kt
+++ common/src/main/kotlin/com/ebicep/chatplus/features/FindMessage.kt
@@ -1,5 +1,6 @@
 package com.ebicep.chatplus.features
 
+import com.ebicep.chatplus.ChatPlus
 import com.ebicep.chatplus.config.Config
 import com.ebicep.chatplus.events.EventBus
 import com.ebicep.chatplus.events.Events
@@ -16,6 +17,9 @@ import com.ebicep.chatplus.features.textbarelements.TranslateToggleEvent
 import com.ebicep.chatplus.hud.*
 import com.ebicep.chatplus.mixin.IMixinChatScreen
 import com.ebicep.chatplus.mixin.IMixinScreen
+import com.ebicep.chatplus.util.ComponentUtil
+import com.ebicep.chatplus.util.GraphicsUtil.fill0
+import net.minecraft.ChatFormatting
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.screens.ChatScreen
 import java.awt.Color
@@ -23,9 +27,12 @@ import java.awt.Color
 object FindMessage {
 
     val FIND_COLOR = Color(255, 255, 85, 255).rgb
-    private val findBackgroundColor = Color(FIND_COLOR).darker().rgb
-    var findEnabled: Boolean = false
+    private val FIND_BACKGROUND_COLOR = Color(FIND_COLOR).darker().rgb
+    var findMode: FindMode = FindMode.OFF
+    val findEnabled: Boolean
+        get() = findMode != FindMode.OFF
     private var lastInput = ""
+    private var lastInputRegex = Regex("")
 
     init {
         var lastMovedToMessage: Pair<Pair<ChatTab.ChatPlusGuiMessage, Int>, Long>? = null // <linked message, wrapped index>, tick
@@ -50,16 +57,24 @@ object FindMessage {
         }
         EventBus.register<ChatScreenCloseEvent> {
             if (findEnabled) {
-                findEnabled = false
+                findMode = FindMode.OFF
                 ChatManager.globalSelectedTab.resetFilter()
             }
         }
         EventBus.register<ChatTabRewrapDisplayMessages> {
-            findEnabled = false
+            findMode = FindMode.OFF
             ChatManager.globalSelectedTab.resetFilter()
         }
         EventBus.register<ChatTabRefreshDisplayMessages> {
             if (findEnabled && lastInput.isNotEmpty()) {
+                if (findMode == FindMode.REGEX) {
+                    try {
+                        lastInputRegex = Regex(lastInput)
+                    } catch (e: Exception) {
+                        ChatPlus.sendMessage(ComponentUtil.literal("Invalid Regex", ChatFormatting.RED))
+                        return@register
+                    }
+                }
                 it.predicates.add { guiMessage ->
                     guiMessage.guiMessage.content.string.contains(lastInput, ignoreCase = true)
                 }
@@ -108,7 +123,7 @@ object FindMessage {
         }
         EventBus.register<TranslateToggleEvent> {
             if (findEnabled) {
-                findEnabled = false
+                findMode = FindMode.OFF
                 ChatManager.globalSelectedTab.resetFilter()
             }
         }
@@ -120,7 +135,7 @@ object FindMessage {
                 ChatManager.globalSelectedTab.getHoveredOverMessageLine(it.mouseX, it.mouseY)?.let { message ->
                     val linkedMessage = message.linkedMessage
                     lastMovedToMessage = Pair(Pair(linkedMessage, message.wrappedIndex), Events.currentTick + 60)
-                    findEnabled = false
+                    findMode = FindMode.OFF
                     ChatManager.globalSelectedTab.moveToMessage(it.screen, message)
                 }
             }
@@ -135,14 +150,50 @@ object FindMessage {
                 if (message.second < Events.currentTick) {
                     return@let
                 }
-                it.backgroundColor = findBackgroundColor
+                it.backgroundColor = FIND_BACKGROUND_COLOR
+            }
+        }
+        EventBus.register<ChatRenderLineTextEvent>({ -5 }) {
+            if (findEnabled && it.chatWindow.tabSettings.selectedTab.wasFiltered) {
+                val chatPlusGuiMessageLine = it.chatPlusGuiMessageLine
+                val line = it.line
+                val guiGraphics = it.guiGraphics
+                val verticalChatOffset = it.verticalChatOffset
+                val renderer = it.chatWindow.renderer
+                val internalX = renderer.internalX
+                val scale = renderer.scale
+                val lineHeight = renderer.lineHeight
+
+                val mode = if (findMode == FindMode.DEFAULT) {
+                    FindMode.REGEX//Config.values.findMessageDefaultMode
+                } else {
+                    findMode
+                }
+
+                val ranges =
+                    if (mode == FindMode.REGEX) ComponentUtil.getWidthRanges(line.content, chatPlusGuiMessageLine.content, lastInputRegex)
+                    else ComponentUtil.getWidthRange(line.content, chatPlusGuiMessageLine.content, lastInput)
+                ranges.forEach {
+                    guiGraphics.fill0(
+                        internalX / scale + it.first,
+                        verticalChatOffset - lineHeight.toFloat(),
+                        internalX / scale + it.second,
+                        verticalChatOffset,
+                        Color(255, 255, 85, 200).rgb
+                    )
+                }
+
             }
         }
     }
 
-    fun toggle(chatPlusScreen: ChatScreen) {
+    fun toggle(chatPlusScreen: ChatScreen, newFindMode: FindMode = FindMode.DEFAULT) {
         chatPlusScreen as IMixinChatScreen
-        findEnabled = !findEnabled
+        findMode = if (findEnabled) {
+            FindMode.OFF
+        } else {
+            newFindMode
+        }
         EventBus.post(FindToggleEvent(findEnabled))
         chatPlusScreen.initial = chatPlusScreen.input!!.value
         lastInput = chatPlusScreen.input?.value ?: ""
@@ -153,6 +204,13 @@ object FindMessage {
         }
         chatPlusScreen as IMixinScreen
         chatPlusScreen.callRebuildWidgets()
+    }
+
+    enum class FindMode {
+        OFF,
+        DEFAULT,
+        REGEX,
+        CONTAINS,
     }
 
 }
