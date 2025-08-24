6c2519dc057dee9aea5776e91eb321616532c947
ebicep <ebicep314@gmail.com>
2025-08-24 01:43:12 -0400

    refactor: updating server tab settings logic from ClientConfigurationPacketListenerImpl to ClientHandshakePacketListenerImpl

--- common/src/main/java/com/ebicep/chatplus/mixin/MixinClientHandshakePacketListenerImpl.java
+++ common/src/main/java/com/ebicep/chatplus/mixin/MixinClientHandshakePacketListenerImpl.java
@@ -3,27 +3,36 @@ package com.ebicep.chatplus.mixin;
 import com.ebicep.chatplus.config.Config;
 import com.ebicep.chatplus.features.chatwindows.ChatWindow;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientConfigurationPacketListenerImpl;
-import net.minecraft.client.multiplayer.CommonListenerCookie;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.ClientHandshakePacketListenerImpl;
+import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.network.Connection;
+import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientConfigurationPacketListenerImpl.class)
-public class MixinClientConfigurationPacketListenerImpl {
+import java.time.Duration;
+import java.util.function.Consumer;
+
+@Mixin(ClientHandshakePacketListenerImpl.class)
+public class MixinClientHandshakePacketListenerImpl {
 
     @Inject(method = "<init>", at = @At(value = "RETURN"))
     private void chatPlus$init(
-            Minecraft minecraft,
             Connection connection,
-            CommonListenerCookie commonListenerCookie,
+            Minecraft minecraft,
+            ServerData serverData,
+            Screen screen,
+            boolean bl,
+            Duration duration,
+            Consumer<Component> consumer,
             CallbackInfo ci
     ) {
-        if (commonListenerCookie.serverData() != null) {
-            String ip = commonListenerCookie.serverData().ip;
+        if (serverData != null) {
+            String ip = serverData.ip;
             for (@NotNull ChatWindow chatWindow : Config.INSTANCE.getValues().getChatWindows()) {
                 chatWindow.getRenderer().updateCachedDimension();
                 chatWindow.getTabSettings().updateTabSettings(ip);
